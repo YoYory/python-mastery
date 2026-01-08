f7edefdae3f990f4f69cc644f6f94a8577244daa
YVC <yoeri.vancauteren@sbe.be>
2026-01-08 10:21:26 +0100

    ex 2.1 finished

--- readrides.py
+++ readrides.py
@@ -1,24 +1,70 @@
 import csv
+from collections import namedtuple
 
-def read_rides_as_tuples(filename):
-    '''
-    Read the bus ride data as a list of tuples
-    '''
+Row = namedtuple("Row", ["route", "date", "daytype", "rides"])
+
+
+class Rowclass:
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+class Rowclass_slots:
+    __slots__ = ["route", "date", "daytype", "rides"]
+
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+def create_record(route, date, daytype, rides, record_type):
+    if record_type == "tuple":
+        return (route, date, daytype, rides)
+    if record_type == "dict":
+        return {
+            "route": route,
+            "date": date,
+            "daytype": daytype,
+            "rides": rides,
+        }
+    if record_type == "class":
+        return Rowclass(route, date, daytype, rides)
+    if record_type == "namedtuple":
+        return Row(route, date, daytype, rides)
+    if record_type == "class_slots":
+        return Rowclass_slots(route, date, daytype, rides)
+
+
+def read_rides(filename, record_type="tuple"):
+    """
+    Read the bus ride data as a list of records
+    """
     records = []
     with open(filename) as f:
         rows = csv.reader(f)
-        headings = next(rows)     # Skip headers
+        headings = next(rows)  # Skip headers
         for row in rows:
             route = row[0]
             date = row[1]
             daytype = row[2]
             rides = int(row[3])
-            record = (route, date, daytype, rides)
+            record = create_record(route, date, daytype, rides, record_type)
             records.append(record)
     return records
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    import sys
     import tracemalloc
+
     tracemalloc.start()
-    rows = read_rides_as_tuples('Data/ctabus.csv')
-    print('Memory Use: Current %d, Peak %d' % tracemalloc.get_traced_memory())
+    if len(sys.argv) == 2:
+        rows = read_rides("Data/ctabus.csv", sys.argv[1])
+    else:
+        rows = read_rides("Data/ctabus.csv", "tuple")
+    print("Memory Use: Current %d, Peak %d" % tracemalloc.get_traced_memory())
