589be0e3fedc25b554be30cf37695787cb5dadf9
YVC <yoeri.vancauteren@sbe.be>
2026-01-15 23:50:05 +0100

    ex 3.3

--- reader.py
+++ reader.py
@@ -1,14 +1,17 @@
 import csv
 from collections.abc import Sequence
+
+
 def read_csv_as_dicts(filename: str, types: list[str]):
     with open(filename) as f:
         result = []
         rows = csv.reader(f)
         headings = next(rows)
         for row in rows:
-            record = {name: func(val) for name, func, val in zip(headings,types,row)}
+            record = {name: func(val) for name, func, val in zip(headings, types, row)}
             result.append(record)
     return result
+
 
 class DataCollection(Sequence):
     def __init__(self, headings):
@@ -24,15 +27,20 @@ class DataCollection(Sequence):
         elif isinstance(item, slice):
             start, stop, step = item.indices(len(self))
             return [
-                dict(zip(self.data.keys(), [self.data[column][i] for column in self.data]))
+                dict(
+                    zip(
+                        self.data.keys(), [self.data[column][i] for column in self.data]
+                    )
+                )
                 for i in range(start, stop, step)
             ]
-        
+
     def append(self, d):
         for key in d:
             self.data[key].append(d[key])
 
-def read_csv_as_columns(filename:str, types:list):
+
+def read_csv_as_columns(filename: str, types: list):
     """
     Read a csv into lists, representing columns
     """
@@ -45,11 +53,27 @@ def read_csv_as_columns(filename:str, types:list):
             records.append(record)
     return records
 
-if __name__ == '__main__':
+
+def read_csv_as_instances(filename: str, cls: type):
+    """
+    Read a CSV file into a list of instances
+    """
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)
+        for row in rows:
+            records.append(cls.from_row(row))
+    return records
+
+
+if __name__ == "__main__":
     import tracemalloc
+
     tracemalloc.start()
     from sys import intern
-    rows = read_csv_as_columns('Data/ctabus.csv', [intern,intern,intern,int])
+
+    rows = read_csv_as_columns("Data/ctabus.csv", [intern, intern, intern, int])
     print(len(rows))
     print(rows[0])
     print(rows[10])
--- stock.py
+++ stock.py
@@ -2,10 +2,16 @@ import csv
 
 
 class Stock:
+    types = (str, int, float)
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        return cls(*[func(val) for func, val in zip(cls.types, row)])
 
     def cost(self):
         return self.shares * self.price
@@ -14,24 +20,8 @@ class Stock:
         self.shares -= nshares
 
 
-def read_portfolio(filename: str):
-    stocks = []
-    types = [str, int, float]
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headings = next(rows)
-        for row in rows:
-            row = [func(val) for func, val in zip(types, row)]
-            stocks.append(Stock(*row))
-    return stocks
-
-
 def print_portfolio(port: list[Stock]):
     print("%10s %10s %10s" % ("name", "shares", "price"))
     print(("-" * 10 + " ") * 3)
     for s in port:
         print("%10s %10d %10.2f" % (s.name, s.shares, s.price))
-
-if __name__ == '__main__':
-    portfolio = read_portfolio("Data/portfolio.csv")
-    print_portfolio(portfolio)
